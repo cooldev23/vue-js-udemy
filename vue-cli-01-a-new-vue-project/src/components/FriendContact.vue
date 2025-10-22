ffad1a2c0018fbddc5e7ef7500a86366718c1f96
Sean Cordill <scordill23@gmail.com>
2025-10-21 21:19:10 -0600

    Components

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong>{{ friend.phone }}</li>
+            <li><strong>Email: </strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        friend: Object
+    },
+    data() {
+        return {
+            detailsAreVisible: false
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+}
+</script>
